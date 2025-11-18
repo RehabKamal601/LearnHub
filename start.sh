9783430a312143b6cb9159cb1eb774594957a410
Rehab Kamal <wwwrehabkamal601@gmail.com>
2025-11-18 18:57:07 +0200

    Add start script for Railway

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd laravel
+composer install --no-dev --optimize-autoloader
+php artisan key:generate
+php artisan config:cache
+php artisan route:cache
+php artisan migrate --force
+php artisan serve --host=0.0.0.0 --port=$PORT
