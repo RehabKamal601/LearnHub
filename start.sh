611f6c381b1aace8b59f7477097e23131468d110
Rehab Kamal <wwwrehabkamal601@gmail.com>
2025-11-18 18:59:09 +0200

    Add Railway configuration

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd laravel
-composer install --no-dev --optimize-autoloader
-php artisan key:generate
-php artisan config:cache
-php artisan route:cache
-php artisan migrate --force
-php artisan serve --host=0.0.0.0 --port=$PORT
